0744c367112fdd59e11af22a661875f4f41eb69a
bawfng04 <nguyendinhbang53az@gmail.com>
2025-04-04 15:34:16 +0700

    .

--- frontend/src/pages/client/Homepage/ServiceUnit.js
+++ frontend/src/pages/client/Homepage/ServiceUnit.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-const ServiceUnit = ({ img, title, description }) => {
+const ServiceUnit = ({ img, title, description, features = [], icon }) => {
   return (
     <div className="service">
-      <h3 className="service-title">{title}</h3>
-      <img src={img} alt={title} className="service-image" />
-      <p className="service-description">{description}</p>
+      <div className="service-image-container">
+        <img src={img} alt={title} className="service-image" />
+      </div>
+      <div className="service-content">
+        <div className="service-icon">
+          <i className={icon}></i>
+        </div>
+        <h3 className="service-title">{title}</h3>
+        <p className="service-description">{description}</p>
+      </div>
     </div>
   );
 };
