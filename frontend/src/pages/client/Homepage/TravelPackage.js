745f4a9f44cc3539ddf9fd6151ae5ef882a2751d
bawfng04 <nguyendinhbang53az@gmail.com>
2025-04-22 21:40:27 +0700

    complete manage travel-package for homepage

--- frontend/src/pages/client/Homepage/TravelPackage.js
+++ frontend/src/pages/client/Homepage/TravelPackage.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import f1 from "../../../assets/f1.jpg";
 import uefa from "../../../assets/uefa.jpg";
 import christmas from "../../../assets/christmas.webp";
+import { GET_TRAVEL_PACKAGES_API, API_URL } from "../../../bang_config/apis";
 
 const TravelPackage = () => {
+
+  const [fetchedPackage, setFetchedPackage] = useState([]);
+
+  const fetchTravelPackages = async () => {
+    try {
+      const response = await fetch(GET_TRAVEL_PACKAGES_API);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      // xử lí thêm cái đường dẫn cho image
+      const processedData = data.data.map((pkg) => ({
+        ...pkg, // giữ nguyên các thuộc tính khác
+        fullImagePath: pkg.package_image? `${API_URL}/${pkg.package_image}` : null,
+      }))
+      console.log("data: ", data);
+      console.log("processedData: ", processedData);
+      setFetchedPackage(processedData);
+    } catch (error) {
+      console.error("Error fetching travel packages:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchTravelPackages();
+  }, [])
+
   return (
     <div className="travel-package-container">
       <h2 className="travel-package-title">Looking for a travel package?</h2>
@@ -42,6 +70,25 @@ const TravelPackage = () => {
           </p>
         </div>
       </div>
+      {Array.isArray(fetchedPackage) &&
+        fetchedPackage.map((pkg, index) => (
+          <div className="travel-packages" key={index}>
+            <div className="travel-package-unit" key={pkg.id || pkg.name}>
+              {" "}
+              <img
+                src={pkg.fullImagePath || "placeholder.jpg"} // Có ảnh dự phòng là tốt
+                alt={pkg.name}
+                className="travel-package-image"
+              />
+              <h3 className="travel-package-name">
+                {pkg.package_name || "Unnamed Package"}
+              </h3>{" "}
+              <p className="travel-package-description">
+                {pkg.package_description || "No description available."}
+              </p>{" "}
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
