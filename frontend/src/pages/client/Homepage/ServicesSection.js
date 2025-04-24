7f32fc103403e71c29441ce8b4e517ae597247bc
bawfng04 <nguyendinhbang53az@gmail.com>
2025-04-24 14:12:12 +0700

    complete manage service

--- frontend/src/pages/client/Homepage/ServicesSection.js
+++ frontend/src/pages/client/Homepage/ServicesSection.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import inflight from "../../../assets/inflight-entertaining.jpg";
 import comfortable from "../../../assets/comfortable-sitting.jpg";
 import meal from "../../../assets/meal.webp";
 import ServiceUnit from "./ServiceUnit";
+import {API_URL, GET_SERVICES_API} from "../../../bang_config/apis";
 
 const ServicesSection = () => {
+  const [fetchedServices, setFetchedServices] = useState([]);
+
+
+  const fetchServices = async () => {
+    const response = await fetch(GET_SERVICES_API, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log("Fetched services:", data);
+    if (response.ok) {
+          let services = data.data;
+          // xử lí đường dẫn ảnh
+      services = services.map((service) => {
+            const fullImagePath = service.service_image ? `${API_URL}/${service.service_image}` : null;
+            return {
+              ...service,
+              service_fullImagePath: fullImagePath,
+            };
+          });
+      setFetchedServices(services);
+      console.log("Fetched services:", services);
+    } else {
+      console.error("Failed to fetch services:", data.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchServices();
+  }, [])
+
   return (
     <section className="services-section">
       <h2 className="services-title">Our Premium Services</h2>
@@ -49,6 +83,20 @@ const ServicesSection = () => {
             "Complimentary snacks",
           ]}
         />
+        {
+          fetchedServices.map((ser) => {
+            return (
+              <ServiceUnit
+                key={ser.id}
+                img={ser.service_fullImagePath}
+                title={ser.service_title}
+                description={ser.service_description}
+                icon="fas fa-utensils"
+                features={[]}
+              />
+            );
+          })
+        }
       </div>
     </section>
   );
